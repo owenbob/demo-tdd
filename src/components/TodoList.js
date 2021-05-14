4903656633d15871e56e1262e8130fde1689be45
owen byomuhangi <owen.byomuhangi@leveltenenergy>
2021-05-14 12:12:34 +0300

    Add TodoList component and test

---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const TodoList = ({ todos }) => (
+    <div>
+        {todos.map((t, i) => (
+            <div key={i}> {t.title} </div>
+        ))}
+    </div>
+);
+
+export default TodoList;
